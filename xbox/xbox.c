dbaf7c1b0f113bf69786ed3d17b3db12b8615246
Jannik Vogel <email@jannikvogel.de>
2020-05-23 01:03:56 +0200

    set mipmap level settings in texture units (FIXME: wrong values)

--- xbox/xbox.c
+++ xbox/xbox.c
@@ -1191,9 +1191,9 @@ static void setup_textures() {
     unsigned int context_dma = 2; //FIXME: Which one did pbkit use?
     XguBorderSrc border = XGU_SOURCE_COLOR;
 
-    unsigned int mipmap_levels = 1;
-    unsigned int min_lod = 0;
-    unsigned int max_lod = 0;
+    unsigned int mipmap_levels = MAX(tx->width_shift, tx->height_shift) + 1;
+    unsigned int min_lod = 2;
+    unsigned int max_lod = 2;
     unsigned int lod_bias = 0;
 
     p = xgu_set_texture_offset(p, i, (uintptr_t)tx->data & 0x03ffffff);
